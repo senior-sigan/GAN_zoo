22ee8ce08ba59457f7ee5ba780ca6c25e89b7904
Ilya Siganov <ilya.blan4@gmail.com>
2020-11-20 01:43:09 +0600

    use config class to set train params

--- gans_zoo/dcgan/main.py
+++ gans_zoo/dcgan/main.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 import pytorch_lightning as pl
 from pl_bolts.callbacks import LatentDimInterpolator, \
     TensorboardGenerativeModelImageSampler
@@ -7,14 +9,18 @@ from torchvision.transforms import transforms
 from gans_zoo.data import ImagesFolder
 from gans_zoo.dcgan.trainer import LitDCGAN
 
-data_dir = "/Volumes/Media/datasets/crop-cosplayers"
-image_size = 64
-batch_size = 32
-workers = 1
-epochs = 100
+
+@dataclass
+class Config:
+    data_dir: str = "/Volumes/Media/datasets/crop-cosplayers"
+    image_size: int = 64
+    batch_size: int = 32
+    workers: int = 1
+    epochs: int = 100
 
 
 def main():
+    config = Config()
     pl.seed_everything(42)
 
     model = LitDCGAN()
@@ -24,25 +30,25 @@ def main():
     ]
 
     trainer = pl.Trainer(
-        max_epochs=epochs,
+        max_epochs=config.epochs,
         callbacks=callbacks,
         fast_dev_run=True,
     )
 
     transform = transforms.Compose([
-        transforms.Resize(image_size),
-        transforms.CenterCrop(image_size),
+        transforms.Resize(config.image_size),
+        transforms.CenterCrop(config.image_size),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
 
     dataset = ImagesFolder(
-        root=data_dir,
+        root=config.data_dir,
         transform=transform
     )
     dataloader = DataLoader(
-        dataset, batch_size=batch_size,
-        shuffle=True, num_workers=workers,
+        dataset, batch_size=config.batch_size,
+        shuffle=True, num_workers=config.workers,
     )
     trainer.fit(model, train_dataloader=dataloader)
 
