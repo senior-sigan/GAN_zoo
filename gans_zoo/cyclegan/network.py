1674a276402962581fcc6859d63e67277d616e31
Ilya Siganov <ilya.blan4@gmail.com>
2020-11-28 21:58:36 +0600

    add resnet generator

--- gans_zoo/cyclegan/network.py
+++ gans_zoo/cyclegan/network.py
@@ -1,3 +1,4 @@
+import functools
 from typing import Optional, Tuple
 
 import torch
@@ -214,3 +215,209 @@ class Discriminator(nn.Module):
     @staticmethod
     def patch_size(height: int, width: int) -> Tuple[int, int, int]:
         return 1, height // 2 ** 4, width // 2 ** 4
+
+
+class ResnetBlock(nn.Module):
+    """Define a Resnet block"""
+
+    def __init__(
+        self,
+        channels: int,
+        padding_type: str,
+        norm_layer,
+        use_dropout: bool,
+        use_bias: bool,
+    ):
+        """
+        Construct the Resnet block.
+
+        A resnet block is a conv block with skip connections
+        We construct a conv block with build_conv_block function,
+        and implement skip connections in <forward> function.
+        Original Resnet paper: https://arxiv.org/pdf/1512.03385.pdf
+
+
+        :param channels: the number of channels in the conv layer.
+        :param padding_type: the name of padding layer:
+            reflect | replicate | zeros
+        :param norm_layer: normalization layer
+        :param use_dropout: if use dropout layers.
+        :param use_bias: if the conv layer uses bias or not
+        """
+        super().__init__()
+
+        block = nn.Sequential()
+        block.add_module('conv_1', nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=3,
+            padding=1,
+            bias=use_bias,
+            padding_mode=padding_type,
+        ))
+        block.add_module('norm_1', norm_layer(channels))
+        block.add_module('activation', nn.ReLU(inplace=True))
+        if use_dropout:
+            block.add_module('dropout', nn.Dropout(0.5))
+        block.add_module('conv_2', nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=3,
+            padding=1,
+            bias=use_bias,
+            padding_mode=padding_type,
+        ))
+        block.add_module('norm_2', norm_layer(channels))
+
+        self.block = block
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Forward function (with skip connections)"""
+        out = x + self.block(x)  # add skip connections
+        return out
+
+
+def upsample_conv_norm_relu(
+    ngf: int,
+    use_bias: bool,
+    norm_layer,
+):
+    # Upsample instead of transposed conv
+    # https://distill.pub/2016/deconv-checkerboard/
+    return nn.Sequential(
+        nn.Upsample(scale_factor=2, mode='nearest'),
+        nn.Conv2d(
+            in_channels=ngf,
+            out_channels=int(ngf / 2),
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            bias=use_bias
+        ),
+        norm_layer(int(ngf / 2)),
+        nn.ReLU(inplace=True)
+    )
+
+
+def conv_norm_relu_block(
+    in_channels: int,
+    out_channels: int,
+    norm_layer,
+    kernel_size: int,
+    stride: int,
+    padding: int,
+    bias: bool,
+    padding_mode: str,
+):
+    return nn.Sequential(
+        nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            padding_mode=padding_mode,
+            bias=bias,
+        ),
+        norm_layer(out_channels),
+        nn.ReLU(True),
+    )
+
+
+class ResnetGenerator(nn.Module):
+    """
+    Resnet-based generator that consists of Resnet blocks
+    between a few downsampling/upsampling operations.
+
+    We adapt Torch code and idea from Justin Johnson's neural style transfer
+    project (https://github.com/jcjohnson/fast-neural-style)
+    """
+
+    def __init__(
+        self,
+        input_nc,
+        output_nc,
+        ngf=64,
+        norm_layer=nn.BatchNorm2d,
+        use_dropout=False,
+        n_blocks=6,
+        padding_type='reflect',
+    ):
+        """
+        Construct a Resnet-based generator.
+
+        :param input_nc: the number of channels in input images
+        :param output_nc: the number of channels in output images
+        :param ngf: the number of filters in the last conv layer
+        :param norm_layer: normalization layer
+        :param use_dropout: if use dropout layers
+        :param n_blocks: the number of ResNet blocks
+        :param padding_type: the name of padding layer in conv layers:
+            reflect | replicate | zero
+        """
+        assert (n_blocks >= 0)
+        super().__init__()
+        if type(norm_layer) == functools.partial:
+            use_bias = norm_layer.func == nn.InstanceNorm2d
+        else:
+            use_bias = norm_layer == nn.InstanceNorm2d
+
+        model = []
+        model += [conv_norm_relu_block(
+            in_channels=input_nc,
+            out_channels=ngf,
+            kernel_size=7,
+            stride=1,
+            padding=3,
+            bias=use_bias,
+            padding_mode=padding_type,
+            norm_layer=norm_layer,
+        )]
+
+        n_downsampling = 2
+        for i in range(n_downsampling):
+            mult = 2 ** i
+            model += [conv_norm_relu_block(
+                in_channels=ngf * mult,
+                out_channels=ngf * mult * 2,
+                kernel_size=3,
+                stride=2,
+                padding=1,
+                bias=use_bias,
+                padding_mode='zeros',
+                norm_layer=norm_layer,
+            )]
+
+        mult = 2 ** n_downsampling
+        for i in range(n_blocks):
+            model += [ResnetBlock(
+                ngf * mult,
+                padding_type=padding_type,
+                norm_layer=norm_layer,
+                use_dropout=use_dropout,
+                use_bias=use_bias,
+            )]
+
+        for i in range(n_downsampling):
+            mult = 2 ** (n_downsampling - i)
+            model += [upsample_conv_norm_relu(
+                ngf=ngf * mult,
+                use_bias=use_bias,
+                norm_layer=norm_layer,
+            )]
+        model += [
+            nn.Conv2d(
+                ngf,
+                output_nc,
+                kernel_size=7,
+                padding=3,
+                padding_mode=padding_type,
+            ),
+            nn.Tanh(),
+        ]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Standard forward"""
+        return self.model(x)
---
+++ tests/cyclegan/test_network.py
@@ -0,0 +1,38 @@
+import pytest
+import torch
+from torch import nn
+
+from gans_zoo.cyclegan.network import ResnetBlock, ResnetGenerator
+
+
+@pytest.mark.parametrize('padding_type', ['reflect', 'replicate', 'zeros'])
+def test_resnet_block(padding_type):
+    block = ResnetBlock(
+        channels=4,
+        padding_type=padding_type,
+        norm_layer=nn.BatchNorm2d,
+        use_dropout=True,
+        use_bias=False,
+    )
+    shape = (2, 4, 16, 16)
+    sample = torch.rand(size=shape)
+    out = block(sample)
+    assert out.shape == shape
+
+
+@pytest.mark.parametrize('padding_type', ['reflect', 'replicate', 'zeros'])
+def test_resnet_generator(padding_type):
+    gen = ResnetGenerator(
+        input_nc=3,
+        output_nc=3,
+        ngf=64,
+        norm_layer=nn.BatchNorm2d,
+        use_dropout=False,
+        n_blocks=6,
+        padding_type=padding_type,
+    )
+    print(gen)
+    shape = (2, 3, 256, 256)
+    sample = torch.rand(size=shape)
+    out = gen(sample)
+    assert out.shape == shape
